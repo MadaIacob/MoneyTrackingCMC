21b7dfc19922387c2ffc25db7e2ec2454bc64125
Cosmin Farcau <farcauco6min@gmail.com>
2015-10-16 11:44:46 +0300

    existsConfigTag in HelperFunctions

--- main/src/HelperFunctions.cpp
+++ main/src/HelperFunctions.cpp
@@ -5,6 +5,7 @@ Date					15.10.2015
 */
 
 #include <string>
+#include <fstream>
 #include "HelperFunctions.h"
 
 using namespace std;
@@ -27,16 +28,69 @@ string cutSign(string validAmount)
 {
 	string cutAmount = "" ;
 	int len = validAmount.length();
-	for(int i = 0; i < len; i++)
-	{
-		if ( !( (i == 0)&&(validAmount[i] == '-') ) )
+	if (validAmount[0] == '-')
+		for(int i = 1; i < len; i++)
 		{
 			cutAmount += validAmount[i] ;
 		}
-		else 
+	else
+	{
+		cutAmount = validAmount ;
+	}	
+	return cutAmount;
+}
+
+// searches for default_wallet in config file and returns true if found
+// and '=' found and some text after '=' is found
+bool existsConfigTag(string configTag)
+{
+	git// open config file
+	ifstream fileToOpen ;
+	fileToOpen.open ("..\\..\\moneytracker.config") ;
+	// value to return
+	bool valid = false ;
+	// while below reads each line in the file and puts it in string lineRead
+	string lineRead ;
+	//n counts lines that are read
+	int n = 0; 
+	while (getline(fileToOpen, lineRead))
+	{
+		n++ ;
+		//position of default_wallet, if found
+		std::size_t foundDef = lineRead.find("default_wallet");
+		if (foundDef!=std::string::npos)
 		{
+			//position of the first char after "default_wallet"
+			int pos = foundDef + 14 ;
+			//check if after default_wallet there is an '=', if a char
+			//different than space or tab enter else
+			while (lineRead[pos] != '=')
+			{
+				if(lineRead[pos] == ' ' || lineRead[pos] == '\t' )
+				{
+					pos++ ;
+				}
+				else
+				{
+					return false ;
+				}
+			}
+			//check if after the '=' some char is found
+			while(!valid && lineRead[pos] != '\n')
+			{
+				if(lineRead[pos] != ' ' && lineRead[pos] != '\t')
+				{
+					pos++ ;
+				}
+				else
+				{
+					valid = true ;
+				}
+			}
 		}
 	}
 	
-	return cutAmount;
+	fileToOpen.close() ;
+	
+	return valid;
 }
