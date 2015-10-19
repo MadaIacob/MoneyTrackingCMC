7e9f0cedd91c55ba0a56ceff26188c35279c3684
Cosmin Farcau <farcauco6min@gmail.com>
2015-10-19 09:38:51 +0300

    HelperFuncTest update

--- main/tst/HelperFunctionsTest.cpp
+++ main/tst/HelperFunctionsTest.cpp
@@ -7,37 +7,25 @@ Date					14.10.2015
 #include "gtest/gtest.h"
 #include "HelperFunctions.h"
 
-TEST(isFirstMinusTest, isMinus)
-{
-	EXPECT_EQ(true, isFirstMinus("-s34d3.5"));
-	EXPECT_EQ(true, isFirstMinus("-00043"));
-	EXPECT_EQ(true, isFirstMinus("--s34d3.5"));
-	EXPECT_EQ(true, isFirstMinus("-@#$.s34d3.5"));
-	EXPECT_EQ(true, isFirstMinus("-+,/s34d3.5"));
-}
-
-TEST(isFirstMinusTest, isNotMinus)
-{
-	EXPECT_EQ(false, isFirstMinus("+-4d3.5"));
-	EXPECT_EQ(false, isFirstMinus("000000.5"));
-	EXPECT_EQ(false, isFirstMinus("s-+9/8.34d3.5"));
-	EXPECT_EQ(false, isFirstMinus("&^@kdd58es34d3"));
-	EXPECT_EQ(false, isFirstMinus("123483703"));
-}
 
 TEST(cutSignTest, cutMinus)
 {
 	EXPECT_EQ("4d3.5", cutSign("-4d3.5"));
 	EXPECT_EQ("-4d3.5", cutSign("--4d3.5"));
 	EXPECT_EQ("#k0.634d3.5", cutSign("-#k0.634d3.5"));
-	EXPECT_EQ("0003435", cutSign("-0003435"));
 	EXPECT_EQ("+343.565", cutSign("-+343.565"));
+	EXPECT_EQ("0003435", cutSign("-0003435"));
+	EXPECT_EQ("0.34365", cutSign("-0.34365"));
+	EXPECT_EQ("0000.0123", cutSign("-0000.0123"));
+	EXPECT_EQ("896123", cutSign("-896123"));
 }
 
-TEST(cutSignTest, noMinus)
+TEST(cutSignTest, cutPlus)
 {
-	EXPECT_EQ("4d3.5", cutSign("4d3.5"));
-	EXPECT_EQ("+00003654d3.5", cutSign("+00003654d3.5"));
-	EXPECT_EQ("#k0.634d3.5", cutSign("#k0.634d3.5"));
-	EXPECT_EQ("0003435", cutSign("0003435"));
+	EXPECT_EQ("4d3.5", cutSign("+4d3.5"));
+	EXPECT_EQ("00003654d3.5", cutSign("+00003654d3.5"));
+	EXPECT_EQ("#k0.634d3.5", cutSign("+#k0.634d3.5"));
+	EXPECT_EQ("3435", cutSign("+3435"));
+	EXPECT_EQ("0.3435", cutSign("+0.3435"));
+	EXPECT_EQ("0000.3435", cutSign("+0000.3435"));
 }
