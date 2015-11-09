52a81eb34c1f2852dd1bdb84526ed3c31e13d890
MadaIacob <iacob.madalina91@yahoo.com>
2015-11-09 14:13:22 +0200

    errors

--- main_classes/inc/MessageHandler.h
+++ main_classes/inc/MessageHandler.h
@@ -1,14 +1,21 @@
+#ifndef MESSAGEHANDLER_H
+#define MESSAGEHANDLER_H
+
+#include "MessageCodes_E.h"
+#include <vector>
+#include <string>
 
 class MessageHandler {
 	private:
 		MessageCodes_E messageCode;
 	
 	public:
-		MessageHandler() : messageCode(NO_ERROR){}
+		MessageHandler();
 		void setMessageCode(MessageCodes_E messageCode);
 		bool isSetMessageCode();
-		void printMessage(vector<string> parameters);
+		void printMessage(std::vector<std::string> parameters);
 		MessageCodes_E getMessageCode();
 		
 };
 
+#endif
