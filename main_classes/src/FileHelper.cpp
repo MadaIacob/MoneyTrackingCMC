9ab876bf278d987846aa920ca1893b3e8f871a99
MadaIacob <iacob.madalina91@yahoo.com>
2015-11-10 10:33:28 +0200

    file helper

---
+++ main_classes/src/FileHelper.cpp
@@ -0,0 +1,56 @@
+#include "FileHelper.h"
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//converts windows path to c++ accepted path 
+//(replaces all '\' characters with '/')
+string convertPath(string givenPath)
+{
+	for(unsigned int i = 0; i < givenPath.length(); i++)
+	{
+		if(givenPath[i] == '\\')
+		{
+			givenPath[i] = '/';
+		}
+	}
+	
+	return givenPath;
+}
+
+//checks if the file fileName already exists. 
+//Returns true if fileName is not already in use and false otherwise
+bool validateFileName(string fileName)
+{
+	//save first position on which '\' was found in fileName
+	size_t firstFound = fileName.find("\\");
+	//if at least one '\' found
+	if(!firstFound)
+	{
+		//replace all '\' with '/' in fileName
+		fileName = convertPath(fileName);
+	}
+	else
+	{
+		//keep fileName unchanged
+	}
+
+	bool isValidFileName = true;
+	
+	ifstream fileChecking(fileName.c_str());
+	
+	//check if fileName already exists
+	if(fileChecking.good())
+	{
+		fileChecking.close();
+		isValidFileName = false;
+	}
+	else
+	{
+		//valid fileName, so keep isValidFileName=true
+	}
+
+	return isValidFileName;
+	
+} 
