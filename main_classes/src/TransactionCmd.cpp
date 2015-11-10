52067c454094ecb9e1caf7b266cf2da1921f7a50
calinpopita <calin.popita@gmail.com>
2015-11-10 17:25:54 +0200

    transactions

---
+++ main_classes/src/TransactionCmd.cpp
@@ -0,0 +1,200 @@
+/*
+File Description		Implementation of WalletCreateCmd class 
+Author					calin-ciprian.popita
+Date					10.11.2015
+*/
+
+#include <string>
+#include <vector>
+#include "TransactionCmd.h"
+#include "HelperFunctions.h"
+#include "Wallet.h"
+#include "WalletEntity.h"
+
+using namespace std;
+
+//constructor - chooses between income/spend
+TransactionCmd::TransactionCmd(const string& command)
+{
+	if(command == "income")
+	{//"income" command 
+		// set transaction sign to (+)
+		walletEntity.setSign("+");
+	}
+	else
+	{//"spend" command 
+		// set transaction sign to (-)
+		walletEntity.setSign("-");
+	}
+}
+
+//performs syntactic analysis - sets error message if anything besides 
+//tags and amount provided for 
+//income/spend  
+void TransactionCmd::parseParams(vector<string>& params) 
+{
+	//check how many parameters were provided for income/spend
+	if (!params.empty()) 
+	{//at least one argument provided for income/spend command
+		//signalises the first category flag found;
+		bool categoryFound = false;
+		//signalises the first wallet flag found;
+		bool walletFound = false;
+		//signalises the first amount found;
+		bool amountFound = false;
+		int i = 0;
+		//go through command line params
+		for(; i < params.size() - 1; i++)
+		{
+			//check for the first "-c" or "--category" flag among parameters
+			if(((params.at(i) == "-c") || 
+			    (params.at(i) == "--category")) &&
+			     categoryFound == false)
+			{
+				//first category flag found
+				categoryFound = true;
+				//jump over "-c" or "--category" flag
+				i++;
+				//put the next command line argument into WalletEntity.category
+				walletEntity.setCategory(params.at(i)); 				
+			}
+			//check for the first "-w" or "--wallet" flag among parameters
+			else if(((params.at(i) == "-w") || 
+			    (params.at(i) == "--wallet")) &&
+			     walletFound == false)
+			{//first wallet flag found
+				walletFound = true;
+				//jump over "-w" or "--wallet" flag
+				i++;
+				//put the next command line argument into Wallet.walletName
+				wallet.setWalletName(params.at(i)); 				
+			}
+			//check for the first amount among parameters
+			else if(!amountFound)
+			{//first unflagged parameter represents the amount
+				//amount found
+				amountFound = true;
+				//put command line argument into WalletEntity.amount
+				walletEntity.setAmount(params.at(i));
+			}
+			//check for unwanted parameters (not flagged params, not amount)
+			else
+			{//unwanted parameter found - set error message
+				ptrMessage->setMessageCode(INVALID_PARAM_ERR);
+			}
+		}
+		// check if there's one more parameter left after search
+		// this is the case where the last but one parameter is not a flag
+		if(i == (params.size() - 1))
+		{//one parameter left after search
+			//check if amount was found so far
+			if(!amountFound)
+			{//the last parameter represents the amount
+				amountFound = true;
+				//put command line argument into WalletEntity.amount
+				walletEntity.setAmount(params.at(i));
+			}
+			else
+			{//unwanted parameter found on last position - set error message
+				ptrMessage->setMessageCode(INVALID_PARAM_ERR);
+			}
+		}
+		else
+		{//no parameters left after search
+		}
+		
+	}
+	else
+	{//no amount provided for income/spend command - set error message
+		ptrMessage->setMessageCode(INVALID_PARAM_ERR);
+	}
+	
+	//rearrange params vector for message printing ?????   **********************************************************************************
+	//empty the vector
+	params.resize(0);
+	//check whether the command is "income" or "spend"
+	if(walletEntity.getSign() == "+")
+	{//the command is "income"
+		//put command name in vector for INVALID_PARAM_ERR case
+		params.push_back("income");
+	}
+	else
+	{//the command is "spend"
+		//put command name in vector for INVALID_PARAM_ERR case
+		params.push_back("spend");
+	}
+}
+
+// validates the values provided for income/spend parameters
+void TransactionCmd::validateParams() 
+{
+	//validate amount - mandatory parameter for transaction
+	//check if amount is valid
+	if(!validateAmount(walletEntity.getAmount().c_str()))
+	{//amount is not valid
+		//set error message
+		ptrMessage->setMessageCode(INVALID_AMOUNT_ERR);
+		
+		//rearrange params vector for message printing ?????   **********************************************************************************
+		//empty the vector
+		params.resize(0);
+		//put amount in vector for INVALID_AMOUNT_ERR case
+		params.push_back(walletEntity.getAmount());	
+		//put amount in vector for INVALID_AMOUNT_ERR case
+		params.push_back(wallet.getWalletName());	
+	}
+	else 
+	{//amount is valid
+		//truncate amount to have only two decimals and no leading zeros
+		string truncatedAmount=truncateAmount(walletEntity.getAmount().c_str());
+		//check if validated sum is negative or zero
+		if(atof(truncatedAmount.c_str()) <= 0.00)								
+		{//amount is valid, but negative or zero 
+			//set error message
+			ptrMessage->setMessageCode(INCOME_SPEND_HIGHER_ERR);
+			//params vector is already set by parseParams method
+		}
+		else
+		{//amount is valid and positive
+			//cut the sign and set walletEntity.amount
+			walletEntity.setAmount(cutSign(truncatedAmount));
+		}
+	}
+}
+
+void TransactionCmd::executeCommand(vector<string>& params)
+{
+	//check if walletName was provided
+	if(wallet.getWalletName() != "")
+	{//walletName provided
+		//check if walletName file exists
+	}
+	else
+	{//walletName not provided 
+		//check for default_wallet tag in config
+		
+		//default_wallet tag exists
+			//check if default_wallet file exists
+			
+			//default_wallet file exists
+				//set default_wallet as walletName 
+				
+			//default_wallet file doesn't exist
+				//set error message
+		
+		//default_wallet tag doesn't exist
+			//set error message 		
+	}
+	
+	//append transaction to walletName file
+	//set success message
+} 
+
+
+
+
+
+
+
+
+
