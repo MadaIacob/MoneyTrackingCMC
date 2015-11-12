654d7758507c1615a2b80f36afc0d1887806e6a9
Cosmin Farcau <farcauco6min@gmail.com>
2015-11-12 17:31:07 +0200

    balance test

--- main_classes/tst/BalanceCmdTest.cpp
+++ main_classes/tst/BalanceCmdTest.cpp
@@ -1,23 +1,81 @@
 #include "gtest/gtest.h"
 
 #include "BalanceCmd.h"
+#include "Command.h"
 #include "Wallet.h"
 #include "WalletEntity.h"
+#include "MessageHandler.h"
+#include "Config.h"
 
+#include <vector>
+#include <string>
 
-TEST(ParseParams, NumberOfParams)
+using namespace std;
+
+TEST(ParseParams, ValidParams)
 {
 	//set-up
+	BalanceCmd balance ;
 	
-	WalletEntity entity1("+","100","RON");
-	Wallet wallet1("testWallet", entity1);
+	vector <string> params0 ;
 	
-	vector <string> params0 {} ;
-	vector <string> params1 {"one"} ;
-	vector <string> params2 {"one", "two"} ;
-	vector <string> params3 {"one", "two", "three"} ;
+	vector <string> params1 ;
+	params1.push_back("-c") ;
+	params1.push_back("two") ;
 	
-	EXPECT_EQ(true, parseParams(params0));
-
-
+	vector <string> params2 ;
+	params2.push_back("--category") ;
+	params2.push_back("two") ;
+	
+	// test
+	EXPECT_EQ(true, balance.parseParams(params0));
+	EXPECT_EQ(true, balance.parseParams(params1));
+	EXPECT_EQ(true, balance.parseParams(params2));
 }
+
+TEST(ParseParams, InvalidParams)
+{
+	//set-up
+	BalanceCmd balance ;
+	
+	vector <string> params1 ;
+	params1.push_back("one") ;
+	
+	vector <string> params2 ;
+	params2.push_back("one") ;
+	params2.push_back("two") ;
+	
+	vector <string> params3 ;
+	params3.push_back("one") ;
+	params3.push_back("two") ;
+	params3.push_back("three") ;
+	
+	EXPECT_EQ(false, balance.parseParams(params1));
+	EXPECT_EQ(false, balance.parseParams(params2));
+	EXPECT_EQ(false, balance.parseParams(params3));
+}
+
+/*
+TEST(ValidateParams, ValidParams)
+{
+	// moneytracker config can not be opened
+	// set-up
+
+	//EXPECT_EQ(COULD_NOT_OPEN_CONFIG_ERR, message.getMessageCode());
+	
+	// no default wallet configured
+	
+	// wallet file can not be opened
+	
+	// category found
+	
+	// category not found
+	
+	
+}
+*/
+
+
+
+
+
