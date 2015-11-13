418b3dada8f479b2119522f78d019fa5be94311c
MadaIacob <iacob.madalina91@yahoo.com>
2015-11-13 10:40:56 +0200

    Trans test

--- main_classes/tst/TransactionsCmdTest.cpp
+++ main_classes/tst/TransactionsCmdTest.cpp
@@ -35,3 +35,39 @@ TEST(TransactionParseTest, noParameters)
     mesS = commandS.getPtrMessage();
     EXPECT_EQ(INVALID_PARAM_ERR, mesS.getMessageCode());
 }
+
+TEST(TransactionParseTest, oneParameter)
+{
+    //set-up
+    TransactionCmd commandI("income");
+    TransactionCmd commandS("spent");
+
+    vector<string> paramsI;
+    paramsI.push_back("123.369");
+    vector<string> paramsI2;
+    paramsI2.push_back("-w");
+    paramsI2.push_back("myWallet");
+    paramsI2.push_back("123.369");
+    vector<string> paramsS;
+    paramsS.push_back("123.369");
+    vector<string> paramsS2;
+    paramsS2.push_back("123.369");
+    paramsS2.push_back("-c");
+    paramsS2.push_back("category");
+    vector<string> paramsS3;
+    paramsS3.push_back("-c");
+    paramsS3.push_back("category");
+
+    MessageHandler mesI;
+    MessageHandler mesS;
+
+    commandI.setMessageHandler(mesI);
+    commandS.setMessageHandler(mesS);
+
+    //test
+    EXPECT_EQ(true, commandI.parseParams(paramsI));
+    EXPECT_EQ(true, commandI.parseParams(paramsI2));
+    EXPECT_EQ(true, commandS.parseParams(paramsS));
+    EXPECT_EQ(true, commandS.parseParams(paramsS2));
+    EXPECT_EQ(false, commandS.parseParams(paramsS3));
+}
